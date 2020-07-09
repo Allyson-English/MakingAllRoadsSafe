c8795eae7c0923a46cb260ba454843476e1b4eb7
Allyson English <58833140+Allyson-English@users.noreply.github.com>
2020-07-08 23:15:52 -0400

    preparing data to conduct ANOVA test of variance

--- MySQL_Connect.py
+++ MySQL_Connect.py
@@ -22,6 +22,18 @@ full_dataset.to_sql(
     if_exists = 'append'
 )
 
+# Cross check most popular roads using SQL (ORDER BY COUNT) and pandas (.value_counts())
+
+full_dataset['road_name'].value_counts()
+popular_roads = '''
+SELECT road_name FROM uber_data GROUP BY road_name ORDER BY COUNT(road_name) DESC LIMIT 5
+'''
+
+# Seems like there should be a more elegant way to transition from a dataframe into a list... *
+pop_rds = pd.read_sql(popular_roads, engine)
+pop_rds = pop_rds.values.tolist()
+pop_rds = [item for sublist in pop_rds for item in sublist]
+
 # Testing out queries by selecting morning commute hours and evening commute hours and manipulating respective dataframes
 
 query = '''
@@ -49,3 +61,24 @@ evening_commute.groupby('road_name').count().sort_values(by=['hour_of_day'], asc
 # This means that it will, in almost all cases, be higher than the recorded speed
 # To circumvent this limitation, instead of doing a two-way t test for difference in means between FFS and traveling speed for the same hour, 
 # it may make more sense to compare average traveling speed for each hour in the day on a given road to identify when, if ever, these speeds differ from one another significantly
+
+
+def data_by_roadway(pop_rd, engine):
+    
+    # Returns dictionary of dataframes for hourly speeds on road indictaed
+    hourlyspeeds_dict = {}
+    
+    temp_dict = {}
+    
+    for i in range(0,24):
+        avrg_spds = f'''SELECT speed_kph_mean FROM uber_data WHERE road_name = '{pop_rd}' AND hour_of_day = {j}'''
+    
+    
+        temp_dict[i] = pd.read_sql(avrg_spds, engine)
+    
+    hourlyspeeds_dict[pop_rd] = temp_dict
+    
+    return hourlyspeeds_dict
+
+Limuru = data_by_roadway("Limuru Road", engine)
+
